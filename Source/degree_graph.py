03d193e10c926686b558f418fd1ab3a80fd8e303
Tiago Teles <teletiago1@hotmail.com>
2021-03-11 17:43:49 +0100

    Updated degree_graph.py

--- Source/degree_graph.py
+++ Source/degree_graph.py
@@ -1,14 +1,19 @@
-import numpy as np
-import time
+"""
+TODO
+"""
+
+# ---------- Imports ---------- #
 import csv
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
 from Degree_function import node_degree, average_degree
-from Adjacency import getMatrices
+from adjacency import get_matrices
 
 
+# ---------- Main Program ---------- #
 # Define directories
-GITDIR = __file__[0:-14]
+GITDIR = __file__[0:-22]
 ASSETDIR = GITDIR + "Assets/"
 DIR2019 = GITDIR + "2019_Filtered/"
 DIR2020  = GITDIR + "2020_Filtered/"
@@ -22,51 +27,53 @@ flightsFile = open(DIR2020 + FLIGHTFILE, encoding="utf8")
 airportFile = open(ASSETDIR + AIRPORTDIR, encoding="utf8")
 
 # Read files
-flightsCSV = csv.reader(flightsFile)
-airportCSV = csv.reader(airportFile)
+flight_csv = csv.reader(flightsFile)
+airport_csv = csv.reader(airportFile)
 
 # Convert flight database from .CSV to List
-flightList = []
-for flight in flightsCSV:
-    flightList.append(flight)
+flight_list = []
+for flight in flight_csv:
+    flight_list.append(flight)
 
-del (flightList[0]) # Remove legend
+del flight_list[0] # Remove legend
 
 # Convert airport codes from .CSV to List
-airportList = []
-for airport in airportCSV:
-    airportList.append(str(airport[1]))
+airport_list = []
+for airport in airport_csv:
+    airport_list.append(str(airport[1]))
 
-del (airportList[0]) # Remove legend
+del airport_list[0] # Remove legend
 
-# Calculate Matrices
-startTime = time.time()
-adjacency, weight = getMatrices(flightList, airportList)
+# Calculate matrices
+adjacency, weight = get_matrices(flight_list)
 
-
+# Calculate degrees
 out_degree_list, in_degree_list, diff_links_list, degree_list = node_degree(weight)
 av_degree = average_degree(degree_list)
 
-for i in range(len(degree_list)):
-    airport = airportList[i]
-    degree = degree_list[i]
-    #print('Aiport =', airport, '+ degree:', degree)
-
-
 zero_els = len(degree_list) - np.count_nonzero(degree_list)
 
-
-occurence = []
+# Calculate probability P(K=i)
+probability = []
 for i in range(1, 300):
-    occurence.append((np.count_nonzero(degree_list > i)))
+    probability.append((np.count_nonzero(degree_list == i)))
 xx = range(1, 300)
 
-occurence_1 = []
+# Calculate probability P(K>i)
+cum_probability = []
 for i in range(1, 300):
-    occurence_1.append((np.count_nonzero(degree_list == i)))
+    cum_probability.append((np.count_nonzero(degree_list >= i)))
 
-plt.plot(xx,occurence, ".")
-plt.plot(xx,occurence_1, ".")
+# Create the plots
+plt.plot(xx,probability, ".")
+plt.plot(xx,cum_probability, ".")
 plt.yscale("log")
 plt.xscale("log")
+
+# Add legend and axis information
+plt.title(FLIGHTFILE)
+plt.xlabel("Node degree, k")
+plt.ylabel("Cumulative probability, P(K(i)>k)")
+
+# Show the plots
 plt.show()
