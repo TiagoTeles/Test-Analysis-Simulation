6d4e54532e9e75967d6dfe7f898389f9cc52618e
Tiago Teles <teletiago1@hotmail.com>
2021-03-11 19:30:27 +0100

    Prevented unwanted running if imported in another module

--- Source/adjacency.py
+++ Source/adjacency.py
@@ -19,7 +19,7 @@ import numpy as np
 def get_matrices(flight_list):
     # ----- Defining the function to find adjacency matrix and weight matrix ----- #
     """ Constructs directed adjacency and weight matrices from the data provided.
-        THe row index corresponds to index of the origin airport, the column index
+        The row index corresponds to index of the origin airport, the column index
         corresponds to the destination airport.
 
     Arguments:
@@ -59,40 +59,41 @@ def get_matrices(flight_list):
 
 
 ## ---------- Main Program ---------- ##
+# Prevents this code form running if the module is imported
+if __name__ == "__main__":
+    # Define directories
+    GIT_DIR = __file__[0:-19]
+    ASSET_DIR = GIT_DIR + "Assets/"
+    DIR_2019 = GIT_DIR + "2019_Filtered/"
+    DIR_2020  = GIT_DIR + "2020_Filtered/"
 
-# Define directories
-GIT_DIR = __file__[0:-19]
-ASSET_DIR = GIT_DIR + "Assets/"
-DIR_2019 = GIT_DIR + "2019_Filtered/"
-DIR_2020  = GIT_DIR + "2020_Filtered/"
+    # Define input file name
+    FLIGHT_DIR = "EU_flights_2019_01.csv"   # .CSV containing list of filtered flights
 
-# Define input file name
-FLIGHT_DIR = "EU_flights_2019_01.csv"   # .CSV containing list of filtered flights
+    # Open files
+    flight_file = open(DIR_2019 + FLIGHT_DIR, encoding="utf8")
 
-# Open files
-flight_file = open(DIR_2019 + FLIGHT_DIR, encoding="utf8")
+    # Read files
+    flight_csv = csv.reader(flight_file)
 
-# Read files
-flight_csv = csv.reader(flight_file)
+    # Convert flight database from .CSV to List
+    flight_list = []
+    for flight in flight_csv:
+        flight_list.append(flight)
 
-# Convert flight database from .CSV to List
-flight_list = []
-for flight in flight_csv:
-    flight_list.append(flight)
-
-del flight_list[0] # Remove legend
+    del flight_list[0] # Remove legend
 
 
-# Calculate Matrices
-start_time = time.time()
-adjacency, weight = get_matrices(flight_list)
+    # Calculate Matrices
+    start_time = time.time()
+    adjacency, weight = get_matrices(flight_list)
 
-# Print matricies
-print("\n")
-print(adjacency)
-print("\n")
-print(weight)
-print("\n")
+    # Print matricies
+    print("\n")
+    print(adjacency)
+    print("\n")
+    print(weight)
+    print("\n")
 
-# Print runtime
-print("Runtime:", round(time.time() - start_time, 1), "s")
+    # Print runtime
+    print("Runtime:", round(time.time() - start_time, 1), "s")
