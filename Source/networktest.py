0c747b9e68590c1a873e69c131948573d608a5e5
ipszczolkowski <79273017+ipszczolkowski@users.noreply.github.com>
2021-03-16 14:19:27 +0100

    Add files via upload
    igraph visualisation and some preliminary analysis

---
+++ Source/networktest.py
@@ -0,0 +1,86 @@
+import igraph as ig
+import time
+import pandas as pd
+import numpy as np
+import csv
+import math
+
+""""Runtime"""
+start_time = time.time()
+
+
+"""Creating a graph from data"""
+data = pd.read_csv("EU_flights_2019_01 (1).csv")
+g0 = ig.Graph.DataFrame(edges=data,directed=True)
+
+
+"""Creating a weighted graph from the adjacency matrix"""
+
+adj_matrix = g0.get_adjacency()
+adj_matrix = list(adj_matrix)
+
+g = ig.Graph.Weighted_Adjacency(adj_matrix)
+g.vs["name"] = g0.vs["name"]
+
+"""Getting the coordinates of airports for the layout"""
+
+airports = []
+coords = []
+
+with open("airports.csv") as file:
+    reader = csv.reader(file, delimiter = ",")
+    i = 0
+    for row in reader:
+        for name in g.vs["name"]:
+            if row[1] == name:
+                airports.append(row[1])
+                coords.append((float(row[-2]),-float(row[-3])))
+                i += 1
+
+
+""""Checks and experiments"""
+
+# print(airports)
+# print(coords)
+# print(len(g.vs["name"]), len(airports))
+
+# print(g)
+# print(adj_matrix)
+# print(np.shape(adj_matrix))
+# print(g0.is_weighted())
+# print(type(adj_matrix))
+
+# print(g.get_adjacency())
+# print(g.is_weighted())
+# print(g.es.attributes())
+
+# for i in range(len(g.vs["name"])):
+#     print(g.vs[i].attributes(),g.degree(i))
+# print(g.vs.select(_degree=g.maxdegree())["name"])
+
+# for j in range(len(g.es["weight"])):
+#     print(int(g.es[j]["weight"]))
+
+# bigedge = g.es.select(weight_gt = 3)
+# print(len(bigedge))
+# print(g.es[67]["weight"])
+# print(g.es[822]["weight"])
+
+
+
+"""Plotting the graph"""
+
+lout = coords
+g.vs["label"] = g.vs["name"]
+
+"""Visual properties of the graph, scalable size of vertices, edges etc."""
+
+visuals = {}
+visuals["vertex_size"] = [10 * math.log(g.degree(i)) for i in g.vs]
+visuals["edge_width"] = [int(weight)//4 for weight in g.es["weight"]]
+visuals["edge_arrow_size"] = 0.5
+
+ig.plot(g, layout = lout, bbox = (5000,5000), **visuals)
+
+""""Runtime"""
+print(time.time() - start_time)
