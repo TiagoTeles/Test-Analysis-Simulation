814d7bd2991dfaf346bcd1210e3949bb07e3357e
Mathieuvds7 <74311370+Mathieuvds7@users.noreply.github.com>
2021-03-23 18:33:40 +0100

    updates + addition time series for degree, need to look at how to find top 10

--- Source/graphs_degree_nodestrength.py
+++ Source/graphs_degree_nodestrength.py
@@ -13,6 +13,39 @@ from degree_function import node_degree, average_degree
 from adjacency import get_matrices
 from Node_strength_function import get_Node_strength
 
+# ---------- Function --------- #
+def get_average_degree(filename):
+    # ----- Defining the function to find the average degree per month ----- #
+    """ Constructs average degree per month (data file)
+
+    Arguments:
+        Filename -- CSV file with flight data
+
+    Returns:
+        Value -- average node degree
+    """
+    flights_file = open(filename, encoding="utf8")
+
+    flights_csv = csv.reader(flights_file)
+
+    flight_list = []
+    for flight in flights_csv:
+        flight_list.append(flight)
+
+    del flight_list[0]  # Remove legend
+
+    # Calculate matrices
+    adjacency, weight, airports = get_matrices(flight_list)
+
+    # Calculate degrees
+    out_degree_list, in_degree_list, diff_links_list, degree_list = node_degree(weight)
+
+    av_degree = average_degree(degree_list)
+
+    return av_degree
+
+
+
 # ---------- Main Program ---------- #
 # Define directories
 GITDIR = __file__[0:-36]
@@ -25,13 +58,16 @@ AIRPORTDIR = "Airports.csv"             # .CSV containing list of EU airports
 FLIGHTFILE = "EU_flights_2020_08.csv"   # .CSV containing list of filtered flights
 
 # Open files
-flightsFile = open(DIR2020 + FLIGHTFILE, encoding="utf8")
 airportFile = open(ASSETDIR + AIRPORTDIR, encoding="utf8")
+flightsFile = open(DIR2020 + FLIGHTFILE, encoding="utf8")
 
 # Read files
-flight_csv = csv.reader(flightsFile)
 airport_csv = csv.reader(airportFile)
+flight_csv = csv.reader(flightsFile)
 
+
+
+''' Get cumulative degree probability for specific month'''
 # Convert flight database from .CSV to List
 flight_list = []
 for flight in flight_csv:
@@ -46,22 +82,22 @@ adjacency, weight, airports = get_matrices(flight_list)
 out_degree_list, in_degree_list, diff_links_list, degree_list = node_degree(weight)
 av_degree = average_degree(degree_list)
 
-#Get top 10
+#Get top 10, only works for 1 month --> indices change for every month
 degree_list = list(degree_list)
 
 degree_sorted = sorted(degree_list)
-top_10 = degree_sorted[:-11:-1]
-
-top_10_index = []
-for i in top_10:
-    top_10_index.append(degree_list.index(i))
-
-top_10_airports = []
-
-for i in top_10_index:
-    top_10_airports.append(airports[i])
-print('The top 10 airports based on degree are: ', top_10_airports)
-print('With their respective degrees: ', top_10,'\n')
+# top_10 = degree_sorted[:-11:-1]
+#
+# top_10_index = []
+# for i in top_10:
+#     top_10_index.append(degree_list.index(i))
+#
+# top_10_airports = []
+#
+# for i in top_10_index:
+#     top_10_airports.append(airports[i])
+# print('The top 10 airports based on degree are: ', top_10_airports)
+# print('With their respective degrees: ', top_10,'\n')
 
 
 #Convert back to array
@@ -78,7 +114,6 @@ xx = range(1, np.max(degree_list))
 cum_probability = []
 for i in range(1, np.max(degree_list)):
     cum_probability.append((np.count_nonzero(degree_list >= i))/len(airports))
-
 
 # Create a power law curve fit
 degree_list = list(degree_list)
@@ -100,7 +135,7 @@ degree_list = np.array(degree_list)
 # Create the plots
 plt.figure()
 #plt.plot(xx,probability, ".")
-plt.subplot(211)
+#plt.subplot(211)
 plt.plot(xx,cum_probability, ".")
 plt.plot(x1,power_law, linestyle = "--")
 plt.yscale("log")
@@ -111,50 +146,60 @@ plt.title("cumulative degree distribution for " + FLIGHTFILE)
 plt.xlabel("Node degree, k")
 plt.ylabel("P(K(i)$\geqslant$k)")
 
-# Show the plots
-#plt.show()
-
-
-
-#Now get the node strengths for the data
-in_strength, out_strength, total_strength = get_Node_strength(weight)
-#print('\n', total_strength)
-
-x2 = np.arange(len(airports))
-
-#Get top 10
-strength_sorted = sorted(total_strength, reverse=True)
-top_10_1 = strength_sorted[0:10]
-
-top_10_index1 = []
-for i in top_10_1:
-    top_10_index1.append(total_strength.index(i))
-
-top_10_airports1 = []
-
-for i in top_10_index1:
-    top_10_airports1.append(airports[i])
-
-print('The top 10 airports based on node strength are: ', top_10_airports1)
-print('With their respective degrees: ', top_10_1)
-
-
-
-# Create the plots
-# This plot shows that there are a lot of airports with a small node strength and only a few with a large node strength
-# This is equivalent to 'Weighted degree' in some definitions
-plt.subplot(212)
-plt.plot(x2,strength_sorted, ".")
-plt.yscale("linear")
-plt.xscale("linear")
-
-# Add legend and axis information
-plt.title("Node strength for the airports in " + FLIGHTFILE)
-plt.xlabel("Just numbers to plot")
-plt.ylabel("Node strength")
-
-# Show the plots
-plt.tight_layout()
+# Show the plot
 plt.show()
+
+''' Time series for average degree'''
+#2019
+
+files_2019 = []
+for i in range(1,13):
+    if i < 10:
+        name = DIR2019 + "EU_flights_2019_0" + str(i) + ".csv"
+    else:
+        name = DIR2019 + "EU_flights_2019_" + str(i) + ".csv"
+
+    files_2019.append(name)
+
+
+av_2019 = []
+
+for i in files_2019:
+    av_2019.append(get_average_degree(i))
+
+
+#2020
+files_2020 = []
+for i in range(1,13):
+    if i < 10:
+        name = DIR2020 + "EU_flights_2020_0" + str(i) + ".csv"
+    else:
+        name = DIR2020 + "EU_flights_2020_" + str(i) + ".csv"
+
+    files_2020.append(name)
+
+av_2020 = []
+
+for i in files_2020:
+    av_2020.append(get_average_degree(i))
+
+
+xx = np.arange(1,13)
+
+# Graph plotting
+#plt.subplot(212)
+plt.figure()
+plt.plot(xx,av_2019,  color = "darkorange", label = "2019")
+plt.plot(xx,av_2020, color = "dodgerblue", label = "2020")
+plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12], ["January","February","March", "April","May","June","July","August", "September", "October", "November", "December"], rotation = 45)
+plt.title("Average node degree per month")
+plt.xlabel("Month")
+plt.ylabel("Average node degree")
+plt.legend()
+plt.grid(axis = "x", linestyle = "--")
+#plt.tight_layout()
+# show plots
+plt.show()
+
 
 
---
+++ Source/graphs_node_strength.py
@@ -0,0 +1,83 @@
+# Imports
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+from degree_function import node_degree, average_degree
+from adjacency import get_matrices
+from Node_strength_function import get_Node_strength
+
+# ---------- Main Program ---------- #
+# Define directories
+GITDIR = __file__[0:-30]
+ASSETDIR = GITDIR + "Assets/"
+DIR2019 = GITDIR + "2019_Filtered/"
+DIR2020  = GITDIR + "2020_Filtered/"
+
+# Define input file name
+AIRPORTDIR = "Airports.csv"             # .CSV containing list of EU airports
+FLIGHTFILE = "EU_flights_2020_08.csv"   # .CSV containing list of filtered flights
+
+# Open files
+airportFile = open(ASSETDIR + AIRPORTDIR, encoding="utf8")
+flightsFile = open(DIR2020 + FLIGHTFILE, encoding="utf8")
+
+# Read files
+airport_csv = csv.reader(airportFile)
+flight_csv = csv.reader(flightsFile)
+
+# Convert flight database from .CSV to List
+flight_list = []
+for flight in flight_csv:
+    flight_list.append(flight)
+
+del flight_list[0] # Remove legend
+
+# Calculate matrices
+adjacency, weight, airports = get_matrices(flight_list)
+
+# Calculate degrees
+out_degree_list, in_degree_list, diff_links_list, degree_list = node_degree(weight)
+av_degree = average_degree(degree_list)
+
+#Now get the node strengths for the data
+in_strength, out_strength, total_strength = get_Node_strength(weight)
+
+#print('\n', total_strength)
+
+x2 = np.arange(len(airports))
+
+# #Get top 10, only works for 1 month --> indices change per month
+strength_sorted = sorted(total_strength, reverse=True)
+# top_10_1 = strength_sorted[0:10]
+#
+# top_10_index1 = []
+# for i in top_10_1:
+#     top_10_index1.append(total_strength.index(i))
+#
+# top_10_airports1 = []
+#
+# for i in top_10_index1:
+#     top_10_airports1.append(airports[i])
+#
+# print('The top 10 airports based on node strength are: ', top_10_airports1)
+# print('With their respective degrees: ', top_10_1)
+
+
+
+# Create the plots
+# This plot shows that there are a lot of airports with a small node strength and only a few with a large node strength
+# This is equivalent to 'Weighted degree' in some definitions
+plt.figure()
+plt.plot(x2,strength_sorted, ".")
+plt.yscale("linear")
+plt.xscale("linear")
+
+# Add legend and axis information
+plt.title("Node strength for the airports in " + FLIGHTFILE)
+plt.xlabel("Just numbers to plot")
+plt.ylabel("Node strength")
+
+# Show the plot
+plt.show()
