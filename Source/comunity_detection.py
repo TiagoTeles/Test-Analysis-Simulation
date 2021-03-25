d4250aadec9856fc85173fbdf1aafc445184b6c0
PhilipVaartjes <PhilipVaartjes@users.noreply.github.com>
2021-03-25 15:54:50 +0100

    finalized community detection

--- Source/comunity_detection.py
+++ Source/comunity_detection.py
@@ -2,58 +2,132 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import igraph as ig
-
-
-g = ig.Graph()
-
-# List of vertices
-vertices = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-#list of edges
-edges = [(0,1),(0,3),(1,3),(2,6),(2,4),(2,5),(3,4),(4,6),(4,5),(5,6),(5,20),(10,20),(9,20),(9,10),(8,9),(9,12),(7,8),(8,11),(12,13),(12,17),(13,19),(18,19),(15,19),(16,19),(14,15),(15,16),(15,17),(4,12),(3,18)]
-
-
-g.add_vertices(21)
-g.add_edges(edges)
+import time
+import pandas as pd
+import numpy as np
+import csv
+import math
 
 def commmunities(graph, method):
+    """"Presenting a number of community detection algorithms"""
 
-    #EDGE_BETWEENNESS most exact but also slowest and most computor heavy
-    #time = 946.8 s (edge) + 353.9 s (vertex)
+    graph = graph.as_undirected("each") #Changing the networks from directed to undirected. The infomap algorithm could not create proper communities, therefore it was decided that the multilevel algorithm should be used instead, this however requires the network to be undirected.
+
+    # EDGE_BETWEENNESS most exact but also slowest and most computer heavy
+    # time = 946.8 s (edge) + 353.9 s (vertex)
     if method == 'edge_betweenness':
-        v = g.community_edge_betweenness()
-        clusters = v.as_clustering()
-    
-    #INFOMAP this one should specifically be for directed graphs but im not sure if we need to detect communities with the graph being directed
+        v = graph.community_edge_betweenness()
+
+    # INFOMAP this one should specifically be for directed graphs but im not sure if we need to detect communities with the graph being directed
     if method == 'infomap':
-        v = g.community_infomap()
-        clusters = v
+        v = graph.community_infomap()
 
-    #FAST_GREEDY & MULITLEVEL (very similar) fast but less accurate
+    # FAST_GREEDY & MULITLEVEL (very similar) fast and accurate, Multilevel should perform very well
     if method == 'fast_greedy':
-        v = g.community_fastgreedy()
-        clusters = v.as_clustering()
+        v = graph.community_fastgreedy()
     elif method == 'multilevel':
-        v = g.community_multilevel()
-        clusters = v
-    
-    #WALKTRAP less fast than FAST_GREEDY but slightly more accurate
+        v = graph.community_multilevel()
+
+    # WALKTRAP less fast than FAST_GREEDY but slightly more accurate
     if method == 'walktrap':
-        v = g.community_walktrap()
-        clusters = v.as_clustering()
-    
-    #LEIDEN algorithm should also be good but only for undirected graphs
+        v = graph.community_walktrap()
+
+    # LEIDEN algorithm should also be good but only for undirected graphs
     if method == 'leiden':
-        v = g.community_leiden()
-        clusters = v
-    
+        v = graph.community_leiden()
+
+    return v
 
 
-    return clusters
+def plot(graph, membership=None):
+    """"This function gives identical styling to each node within one community and it makes the edges between communities gray in color"""
+    if membership is not None:
+        gcopy = graph.copy()
+        edges = []
+        edges_colors = []
+        for edge in graph.es():
+            if membership[edge.tuple[0]] != membership[edge.tuple[1]]:
+                edges.append(edge)
+                edges_colors.append('green')
+            else:
+                edges_colors.append("red")
+        gcopy.delete_edges(edges)
+        layout = gcopy.layout(None)
+        graph.es["color"] = edges_colors
+    else:
+        layout = g.layout(None)
+        graph.es["color"] = "gray"
 
-methods = ["edge_betweenness", "infomap", "fast_greedy", "multilevel", "walktrap", "leiden" ]
-clusters = commmunities(g, methods[5])
+    """"Creating empty lists for the layout of the visualization"""
+    airports = []
+    coords = []
 
-g.vs['label'] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13","14", "15", "16", "17","18", "19", "20", "21"]
-layout = g.layout("kk")
-ig.plot(clusters, layout = layout)
+    """"Reading the coördinates of the airports for the layout"""
+    with open("C:\\Users\\phili\\PycharmProjects\\Test-Analysis-Simulation\\Assets\\Airports.csv",
+              encoding="mbcs") as file:
+        reader = csv.reader(file, delimiter=",")
+        i = 0
+        for row in reader:
+            for name in g.vs["name"]:
+                if row[1] == name:
+                    airports.append(row[1])
+                    coords.append((float(row[-2]), -float(row[-3])))
+                    i += 1
+    lout = coords
+
+    """"Determining the visual aspects of the network"""
+    visual_style = {}
+    visual_style["edge_arrow_size"] = 0.025
+    visual_style["vertex_shape"] = "circle"
+    visual_style['layout'] = lout
+    visual_style["vertex_size"] = [50 * math.log(graph.degree(i)) for i in graph.vs]
+    visual_style['layout'] = lout
+    visual_style["edge_width"] = [0.01 * int(weight) for weight in graph.es["weight"]]
+    visual_style["bbox"] = (15000, 15000)
+    visual_style["margin"] = 40
+    visual_style["edge_label"] = graph.es["weight"]
+    g.vs["label"] = g.vs["name"]
+
+    if membership is not None:
+        #only need the code below for random colors
+        colorsrand = [] #'b','g','r','c','m','y'
+        colors = ['Tomato','Orange', 'DodgerBlue', 'MediumSeaGreen', 'SlateBlue', "Violet", "#FF3399", "#9CCC65", "#00838F", "#795548"]
+        for i in range(0, max(membership) + 1):
+            colorsrand.append('%06X' % np.random.randint(0, 0xFFFFFF))
+        for vertex in graph.vs():
+            # vertex["color"] = str('#') + colorsrand[membership[vertex.index]]
+            vertex["color"] = colors[membership[vertex.index]]
+        visual_style["vertex_color"] = graph.vs["color"]
+    ig.plot(graph, autocurve= False, **visual_style)
+
+if __name__ == "__main__":
+    """"Runtime"""
+    start_time = time.time()
+
+    """"Specifying the filename and location"""
+    filename = "EU_flights_2019_06.csv"
+    path = f'C:\\Users\\phili\\PycharmProjects\\Test-Analysis-Simulation\\2019_Filtered\\{filename}'
+
+    """Creating a graph from data"""
+    data = pd.read_csv(path, usecols=[1, 2])
+    g0 = ig.Graph.DataFrame(edges=data, directed=True)
+
+    """Creating a weighted graph from the adjacency matrix"""
+    adj_matrix = g0.get_adjacency()
+    adj_matrix = list(adj_matrix)
+    g = ig.Graph.Weighted_Adjacency(adj_matrix)
+    g.vs["name"] = g0.vs["name"]
+
+    """"Presenting options for which type of community detection algorithm should be used"""
+    methods = ["edge_betweenness", "infomap", "fast_greedy", "multilevel", "walktrap", "leiden"]
+    clusters = commmunities(g, methods[3]) #According to the research presented in the method section of the paper, we should use the multilevel algorithm
+    print(clusters.modularity) #This number gives a representation of how good the communities have been formed, if this number is close to 0, then the communities which have been found are not good communities
+    print(clusters) #Printing the actual communities
+    membership = clusters.membership
+    plot(g, membership)
+
+    """"Runtime"""
+    print(time.time() - start_time)
+
+# source of code which helped with visualisation
+# https://stackoverflow.com/questions/23184306/draw-network-and-grouped-vertices-of-the-same-community-or-partition
