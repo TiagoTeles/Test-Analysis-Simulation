4cb907dce100544025472296e7fdc010d224527a
Tiago Teles <teletiago1@hotmail.com>
2021-04-29 17:31:37 +0200

    Added network restoration

--- Source/network_restoration.py
+++ Source/network_restoration.py
@@ -1,11 +1,55 @@
+"""
 
-## Month: April 2019/2020
+"""
 
-## Get the entire unaltered graph for 2019/2020
 
-## Determine measure that indicates network restoration (Betweeness, Closeness, Clustering, Degree, etc.)
+# ---------- Imports ---------- #
+from data_visualization import create_graph
+import itertools
+import igraph as ig
+import time
 
-## Determine desired changes to the parameter
+
+# ---------- Setup --------- #
+# Define directories
+GIT_DIR = __file__[0:-29]
+FLIGHT_DIR_2019 = GIT_DIR + "2019_Filtered/EU_flights_2019_04.csv"
+FLIGHT_DIR_2020 = GIT_DIR + "2020_Filtered/EU_flights_2020_04.csv"
+
+# List of airports to try restoration
+TARGETS = ["EGLL", "LFPG", "EDDF", "EHAM", "LEMD"]
+
+
+# ---------- Function Definitions ---------- #
+
+
+# ---------- Main Program ---------- #
+# Start timer
+start_time = time.time()
+
+# Determine graphs
+graph_2019 = create_graph(FLIGHT_DIR_2019)
+graph_2020 = create_graph(FLIGHT_DIR_2020)
+
+# Get icao code pairs
+icao_pairs = itertools.combinations(TARGETS, 2)
+
+# Iterate through edges
+for pair in icao_pairs:
+    vertex_pair = graph_2020.vs.select(name_in = pair)
+
+    for edge in graph_2020.es.select(_within = vertex_pair):
+        print(edge)
+
+    print()
+
+print(time.time() - start_time)
+
+# Change in the parameter required to fully restore the network
+#delta_desired = func(graph_2020) - func(graph_2019)
+
+
+
 
 ## Determine list of edges to be checked (Either top airports or top decrese during the pandemic) (NC2)
 
@@ -14,5 +58,3 @@
 ## Note the effort (Number of flights added) to the edge
 
 ## Determine and rank Percentage chage per flight added
-
-##
